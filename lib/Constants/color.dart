026efc5f0bfe7ab22d773a6692dc47e2030f9658
thenavneeth <132270020+thenavneeth@users.noreply.github.com>
2023-07-05 13:07:33 +0530

    Update color.dart

--- lib/Constants/color.dart
+++ lib/Constants/color.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color moneyBoyPurple = Color.fromARGB(255, 54, 72, 136);
 const Color moneyBoyPurpleLight = Color(0xFF9C91ED);
-const Color primaryColor = Color.fromARGB(255, 54, 72, 136git add );
+const Color primaryColor = Color.fromARGB(255, 54, 72, 136);
